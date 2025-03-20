1f49336307524a4741078a8e3a2049563ddc20ea
srikk <122169588+py-code314@users.noreply.github.com>
2025-03-20 16:29:23 -0400

    fix: bug with images in About Us

--- src/pages/about.js
+++ src/pages/about.js
@@ -13,32 +13,37 @@ const content = document.querySelector('#content');
 const aboutUs = () => {
   // Add subsections for Our Story, About Our Restaurant, and Contact Us
   content.innerHTML += `
-    <div class="tabpanel about-us flex-col-center" role="tabpanel" aria-labelledby="about">
+    <div class="tabpanel about-us flex-col" role="tabpanel" aria-labelledby="about">
 
-      <section class="story" >
-        <div class="story__image-div img-container">
-          <img class="story__image img-full" src="${chefImg}" alt="Chef tasting food">
-        </div>
-        <div class="story__content flex-col-center">
-          <h2 class="story__title section-heading">Our Story</h2>
-          <p class="story__para">Our restaurant is led by a passionate and visionary chef who has dedicated his life to crafting culinary experiences. With a lifelong love for food, he honed his skills in various kitchens before realizing his dream of opening his own restaurant. His mission is to create a space where food brings people together, using only the freshest ingredients to ensure every bite is a testament to his dedication.</p>
+      <section class="story flex-col" >
+        <h2 class="story__title section-heading">Our Story</h2>
+        <div class="story__line">
+          <div class="story__image-div img-container">
+            <img class="story__image img-full" src="${chefImg}" alt="Chef tasting food">
+          </div>
+          <div class="story__content flex-col-center">
+            
+            <p class="story__para">Our restaurant is led by a passionate and visionary chef who has dedicated his life to crafting culinary experiences. With a lifelong love for food, he honed his skills in various kitchens before realizing his dream of opening his own restaurant. His mission is to create a space where food brings people together, using only the freshest ingredients to ensure every bite is a testament to his dedication.</p>
 
-          <p class="story__para">Our chef is a true culinary artist, driven by passion and creativity. He leads a collaborative kitchen where every team member feels valued. We are proud of our current team and are always looking for like-minded individuals who share our passion for food and hospitality. If you're driven by culinary excellence, we invite you to join us in creating unforgettable dining experiences.</p>
+            <p class="story__para">Our chef is a true culinary artist, driven by passion and creativity. He leads a collaborative kitchen where every team member feels valued. We are proud of our current team and are always looking for like-minded individuals who share our passion for food and hospitality. If you're driven by culinary excellence, we invite you to join us in creating unforgettable dining experiences.</p>
+          </div>
         </div>
       </section>
 
-      <section class="about">
-        <div class="about__content flex-col-center">
-          <h2 class="about__title section-heading">About Our Restaurant</h2>
-          <p class="about__para">
-            Located in the heart of Paris, our modern bistro blends sleek sophistication with the timeless charm of French cuisine. As you step inside, you're greeted by a spacious, minimalist interior with floor-to-ceiling windows that offer stunning views of the city. The contemporary design features clean lines and elegant lighting, creating a chic and inviting atmosphere. Our chefs prepare each dish with precision and passion, using locally sourced ingredients to honor traditional French techniques with a modern twist.      
-          </p>
-          <p class="about__para">
-            Our location allows guests to explore the city's iconic landmarks, such as the Eiffel Tower and Notre-Dame Cathedral, just a short stroll away. Whether you're a local or a visitor, our restaurant is the perfect place to experience the authentic flavors and ambiance of Paris. Our staff is dedicated to providing exceptional service, ensuring that every visit is a memorable one. Come and indulge in a culinary journey that will leave you feeling like you're part of the city's dynamic present. 
-          </p>
-        </div>
-        <div class="about__image-div img-container">
-          <img class="about__image img-full" src="${diningImg}" alt="People dining at a restaurant">
+      <section class="about flex-col">
+        <h2 class="about__title section-heading">About Our Restaurant</h2>
+        <div class="about__line">
+          <div class="about__content flex-col-center">         
+            <p class="about__para">
+              Located in the heart of Paris, our modern bistro blends sleek sophistication with the timeless charm of French cuisine. As you step inside, you're greeted by a spacious, minimalist interior with floor-to-ceiling windows that offer stunning views of the city. The contemporary design features clean lines and elegant lighting, creating a chic and inviting atmosphere. Our chefs prepare each dish with precision and passion, using locally sourced ingredients to honor traditional French techniques with a modern twist.      
+            </p>
+            <p class="about__para">
+              Our location allows guests to explore the city's iconic landmarks, such as the Eiffel Tower and Notre-Dame Cathedral, just a short stroll away. Whether you're a local or a visitor, our restaurant is the perfect place to experience the authentic flavors and ambiance of Paris. Our staff is dedicated to providing exceptional service, ensuring that every visit is a memorable one. Come and indulge in a culinary journey that will leave you feeling like you're part of the city's dynamic present. 
+            </p>
+          </div>
+          <div class="about__image-div img-container">
+            <img class="about__image img-full" src="${diningImg}" alt="People dining at a restaurant">
+          </div>
         </div>
       </section>
 
